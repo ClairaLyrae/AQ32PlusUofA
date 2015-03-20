d7d63d8be84a8ece4327d9bc5bac399c9c77957f
ClairaLyrae <csafi@email.arizona.edu>
2015-03-19 21:26:19 -0700

    Added gas sensor board and mlx90614 thermometer sensor interface code. Untested and not yet implemented in main code.

---
+++ src/sensors/gasSensorBoard_I2C.c
@@ -0,0 +1,160 @@
+/*
+ * Gas Sensor Board
+ *
+ * Author:
+ * Claira Safi
+ *
+ * TODO:
+ * Requires testing to see if it works. Also requires testing to make sure
+ * I2C restart is being sent as it it necessary for the mlx sensor to
+ * correctly interpret the read command
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include "board.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define gsbConvFactor 		0.02f
+#define gsbRefConvFactor 	0.02f
+#define gsbHtrConvFactor 	0.02f
+
+#define gsbDefaultAddress 	0x5A
+#define gsbCmdStatus 		0x5A
+#define gsbCmdCatalytic1 	0x5A
+#define gsbCmdCatalyticHtr1 0x5A
+#define gsbCmdCatalyticCon1 0x5A
+#define gsbCmdCatalytic2 	0x5A
+#define gsbCmdCatalyticHtr2 0x5A
+#define gsbCmdCatalyticCon2 0x5A
+#define gsbCmdElectro 		0xF0
+#define gsbCmdElectroRef 	0xF0
+#define gsbCmdElectroCon 	0xF0
+#define gsbCmdCon		 	0xF0
+
+///////////////////////////////////////////////////////////////////////////////
+
+I2C_TypeDef *gsbI2C;
+uint8_t     gsbAddress;
+
+uint16_t gsbRawElectro;
+uint16_t gsbRawCatalytic1;
+uint16_t gsbRawCatalytic2;
+
+uint16_t gsbRawElectroRef;
+uint16_t gsbRawCatalyticHtr1;
+uint16_t gsbRawCatalyticHtr2;
+
+float gsbElectroPPM;
+float gsbCatalytic1PPM;
+float gsbCatalytic2PPM;
+
+uint8_t gsbSensorReady = false;
+
+///////////////////////////////////////////////////////////////////////////////
+// Read Electrochemical Sensor Values
+///////////////////////////////////////////////////////////////////////////////
+
+void readElectrochemicalSensor(void)
+{
+    uint16_t data[2];
+
+    i2cRead(gsbI2C, gsbAddress, gsbCmdElectro, 2, data);    // Request pressure read
+
+    gsbRawElectro = data[0] | (data[1] << 8);
+    gsbElectroPPM = calculatePPM(gsbRawElectro);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Read Catalytic Sensor #1
+///////////////////////////////////////////////////////////////////////////////
+
+void readCatalyticSensor1(void)
+{
+    uint16_t data[2];
+
+    i2cRead(gsbI2C, gsbAddress, gsbCmdCatalytic1, 2, data);
+
+    gsbRawCatalytic1 = data[0] | (data[1] << 8);
+    gsbCatalytic1PPM = calculatePPM(gsbRawCatalytic1);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Read Catalytic Sensor #2
+///////////////////////////////////////////////////////////////////////////////
+
+void readCatalyticSensor2(void)
+{
+    uint16_t data[2];
+
+    i2cRead(gsbI2C, gsbAddress, gsbCmdCatalytic2, 2, data);
+
+    gsbRawCatalytic2 = data[0] | (data[1] << 8);
+    gsbCatalytic2PPM = calculatePPM(gsbRawCatalytic2);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Set Catalytic Sensor #1 Heater Voltage
+///////////////////////////////////////////////////////////////////////////////
+
+void writeCatalyticHtr1(float voltage)
+{
+    uint8_t data[1];
+    data[0] = (unsigned)(voltage / gsbHtrConvFactor);
+    i2cWrite(gsbI2C, gsbAddress, gsbCmdCatalyticHtr1, 1, data);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Set Catalytic Sensor #2 Heater Voltage
+///////////////////////////////////////////////////////////////////////////////
+
+void writeCatalyticHtr2(float voltage)
+{
+    uint8_t data[1];
+    data[0] = (unsigned)(voltage / gsbHtrConvFactor);
+    i2cWrite(gsbI2C, gsbAddress, gsbCmdCatalyticHtr2, 1, data);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Set Electrochemical Sensor Reference Voltage
+///////////////////////////////////////////////////////////////////////////////
+
+void writeElectroRef(float voltage)
+{
+    uint8_t data[1];
+    data[0] = (unsigned)(voltage / gsbRefConvFactor);
+    i2cWrite(gsbI2C, gsbAddress, gsbCmdElectroRef, 1, data);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Calculate PPM Value From Raw
+///////////////////////////////////////////////////////////////////////////////
+
+float calculatePPM(uint16_t rawVal)
+{
+	return (((float)rawVal)*gsbConvFactor);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Sensor Initialization
+///////////////////////////////////////////////////////////////////////////////
+
+uint8_t initSensorGSB(void)
+{
+	uint8_t data[1];
+
+    gsbI2C = I2C2;
+    gsbAddress = 0x5A;
+
+    // Read device status
+    i2cRead(gsbI2C, gsbAddress, gsbCmdStatus, 1, data);
+
+    // Return POR status (if device is active or not)
+    gsbSensorReady = ((data[0] & 0x10) >> 4);
+
+    return gsbSensorReady;
+}
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ src/sensors/mlx90614_I2C.c
@@ -0,0 +1,99 @@
+/*
+ * Gas Sensor Board
+ *
+ * Author:
+ * Claira Safi
+ *
+ * TODO:
+ * Requires testing to see if it works. Also requires testing to make sure
+ * I2C restart is being sent as it it necessary for the mlx sensor to
+ * correctly interpret the read command
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include "board.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define mlxConvFactor 		0.02f
+
+#define mlxDefaultAddress 	0x5A
+#define mlxCmdTempAmbient 	0x06
+#define mlxCmdTempObj1 		0x07
+#define mlxCmdTempObj2 		0x08
+#define mlxCmdReadFlags		0xF0
+
+///////////////////////////////////////////////////////////////////////////////
+
+I2C_TypeDef *mlx90614I2C;
+uint8_t     mlx90614Address;
+
+uint16_t mlxRawAmbTemp;
+uint16_t mlxRawObjTemp;
+
+float mlxObjTempC;
+float mlxAmbTempC;
+
+uint8_t mlxSensorReady = false;
+
+///////////////////////////////////////////////////////////////////////////////
+// Read Ambient Temperature
+///////////////////////////////////////////////////////////////////////////////
+
+void readAmbientTemperature(void)
+{
+    uint8_t data[2];
+
+    i2cRead(mlx90614I2C, mlx90614Address, mlxCmdTempAmbient, 2, data);    // Request pressure read
+
+    mlxRawAmbTemp = data[0];
+    mlxRawAmbTemp |= (data[1] << 8);
+    mlxAmbTempC = calculateMLXTemperature(mlxRawAmbTemp);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Read Object Temperature
+///////////////////////////////////////////////////////////////////////////////
+
+void readObjectTemperature(void)
+{
+    uint8_t data[2];
+
+    i2cRead(mlx90614I2C, mlx90614Address, mlxCmdTempObj1, 2, data);    // Request pressure read
+
+    mlxRawObjTemp = data[0];
+    mlxRawObjTemp |= (data[1] << 8);
+    mlxObjTempC = calculateMLXTemperature(mlxRawObjTemp);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Calculate Temperature From MLX Raw Data
+///////////////////////////////////////////////////////////////////////////////
+
+float calculateMLXTemperature(uint16_t rawTemp)
+{
+	return (((float)rawTemp)*mlxConvFactor) - 273.0f;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Sensor Initialization
+///////////////////////////////////////////////////////////////////////////////
+
+uint8_t initSensorMLX90614(void)
+{
+	uint8_t data[1];
+
+    mlx90614I2C = I2C2;
+    mlx90614Address = 0x5A;
+
+    // Read device flags
+    i2cRead(mlx90614I2C, mlx90614Address, mlxCmdReadFlags, 1, data);
+
+    // Return POR status (if device is active or not)
+    mlxSensorReady = ((data[0] & 0x10) >> 4);
+
+    return mlxSensorReady;
+}
+\
+///////////////////////////////////////////////////////////////////////////////
